abd1e21f653a87b70178a26446e7e976763642bc
baotram1209 <baotramtran.1209@gmail.com>
2022-02-04 10:18:43 +0100

    test-submenu

--- js/nav.js
+++ js/nav.js
@@ -12,3 +12,46 @@ navToggle.addEventListener('click', () => {
       navToggle.setAttribute('aria-expanded', false);
   }
 }) 
+
+
+// Sub-menu
+
+
+const btns = document.querySelectorAll('.btn');
+const dropMenus = document.querySelectorAll('.drop-menu');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        removeActive();
+        btn.classList.add('active');
+        document.querySelector(btn.dataset.target).classList.add('active');
+    })
+})
+
+const removeActive = () => {
+    btns.forEach(btn => btn.classList.remove('active'));
+    dropMenus.forEach(dropmenu => dropmenu.classList.remove('active'));
+}
+
+window.onclick = (e) => {
+    if (!e.target.matches('.btn')) {
+        removeActive()
+    }
+}
+
+
+
+// var dropdown = document.getElementsByClassName("dropdown-btn");
+// var i;
+
+// for (i = 0; i < dropdown.length; i++) {
+//   dropdown[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var dropdownContent = this.nextElementSibling;
+//     if (dropdownContent.style.display === "block") {
+//       dropdownContent.style.display = "none";
+//     } else {
+//       dropdownContent.style.display = "block";
+//     }
+//   });
+// }
